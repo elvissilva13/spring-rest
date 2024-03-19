3224c7c1e3a3fde9bd0579154cd275137c6fee65
Elvis junior da silva <elvisbelbis@gmail.com>
2024-03-18 22:20:56 -0300

    commit

---
+++ src/main/java/com/algaworks/awpag/domain/mode/Cliente.java
@@ -0,0 +1,13 @@
+package com.algaworks.awpag.domain.mode;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+    private  long id;
+    private String nome;
+    private String email;
+    private String telefone;
+}
