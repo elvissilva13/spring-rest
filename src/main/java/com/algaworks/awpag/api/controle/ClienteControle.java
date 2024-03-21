9d0b5150e09c66ca91bce230967869810986c31e
Elvis junior da silva <elvisbelbis@gmail.com>
2024-03-20 23:01:14 -0300

    commit segunda aula

--- src/main/java/com/algaworks/awpag/api/controle/ClienteControle.java
+++ src/main/java/com/algaworks/awpag/api/controle/ClienteControle.java
@@ -1,42 +1,28 @@
 package com.algaworks.awpag.api.controle;
 
 import com.algaworks.awpag.domain.mode.Cliente;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+
+
 @RestController
 public class ClienteControle {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> Listar(){
-        var cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Elvis Silva");
-        cliente1.setEmail("bel-bis@hotmail.com");
-        cliente1.setTelefone("12-2765-2222");
 
-        var cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Ketly cremonese");
-        cliente2.setEmail("KetlySilva@hotmail.com");
-        cliente2.setTelefone("13-2165-1122");
 
-        var cliente3 = new Cliente();
-        cliente3.setId(3L);
-        cliente3.setNome("Sofia maria silva");
-        cliente3.setEmail("SofiaMariaC@hotmail.com");
-        cliente3.setTelefone("14-8765-8769");
-
-        var cliente4 = new Cliente();
-        cliente4.setId(4L);
-        cliente4.setNome("gabriel silva");
-        cliente4.setEmail("gaRodriguesSilva@hotmail.com");
-        cliente4.setTelefone("15-0989-1112");
-
-        return Arrays.asList(cliente1,cliente2,cliente3,cliente4);
+        return manager.createQuery("from Cliente",Cliente.class).getResultList();
 
     }
 }
--- src/main/java/com/algaworks/awpag/domain/mode/Cliente.java
+++ src/main/java/com/algaworks/awpag/domain/mode/Cliente.java
@@ -1,13 +1,23 @@
 package com.algaworks.awpag.domain.mode;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
+@Entity
+//@table(name="tb_cliente")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 public class Cliente {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  long id;
+
     private String nome;
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 }
