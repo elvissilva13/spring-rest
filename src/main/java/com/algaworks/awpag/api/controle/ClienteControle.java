9de2a5d7f3d8f18317e93b50b0a10f987ab3ff40
Elvis junior da silva <elvisbelbis@gmail.com>
2024-03-21 18:36:47 -0300

    commit aula3

--- src/main/java/com/algaworks/awpag/api/controle/ClienteControle.java
+++ src/main/java/com/algaworks/awpag/api/controle/ClienteControle.java
@@ -1,9 +1,12 @@
 package com.algaworks.awpag.api.controle;
 
 import com.algaworks.awpag.domain.mode.Cliente;
+import com.algaworks.awpag.doman.repository.ClienteRepository;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,18 +14,23 @@ import java.util.Arrays;
 import java.util.List;
 
 
-
+@AllArgsConstructor
 @RestController
 public class ClienteControle {
+   // @Autowired
+    private final ClienteRepository clienteRepository;
 
-    @PersistenceContext
-    private EntityManager manager;
+
+
 
     @GetMapping("/clientes")
     public List<Cliente> Listar(){
+        // return clienteRepository.findAll(); pesquisa tudo no banco de dados
+        //return  clienteRepository.findByNome("elvis");pesquisa exatamente  igual o nome
+        return clienteRepository.findByNomeContaining("k");
 
 
-        return manager.createQuery("from Cliente",Cliente.class).getResultList();
+    //    return manager.createQuery("from Cliente",Cliente.class).getResultList();
 
     }
 }
---
+++ src/main/java/com/algaworks/awpag/doman/repository/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.algaworks.awpag.doman.repository;
+
+import com.algaworks.awpag.domain.mode.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+
+   // List<Cliente> findByNome(String nome);pesquisa extamente o nome igual
+
+
+    List<Cliente> findByNomeContaining(String nome);//pesquisa generica
+}
