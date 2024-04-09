e4a6cdde12ca00542158273da3eeb783409b0701
Elvis junior da silva <elvisbelbis@gmail.com>
2024-04-08 22:31:42 -0300

    commit

--- src/main/java/com/algaworks/awpag/domain/mode/exception/NegocioException.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.awpag.domain.mode.exception;
-
-public class NegocioException extends RuntimeException{
-    public NegocioException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/algaworks/awpag/doman/repository/ParcelamentoRepository.java
+++
@@ -1,4 +0,0 @@
-package com.algaworks.awpag.doman.repository;
-
-public class ParcelamentoRepository {
-}
